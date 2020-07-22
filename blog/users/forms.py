bef9e1618affdab632cfa0998d7f1dd69a42c602
Yiaoma <a.o.kalugin@gmail.com>
2020-07-22 16:26:32 +0100

    Created layout for registration form.

--- blog/users/forms.py
+++ blog/users/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class LoginForm(FlaskForm):
     email = StringField(
@@ -12,3 +12,22 @@ class LoginForm(FlaskForm):
         validators=[DataRequired(), Length(min=8, max=32)]
     )
     submit = SubmitField("Login")
+
+class RegisterForm(FlaskForm):
+    username = StringField(
+        "Username",
+        validators=[DataRequired(), Length(min=5, max=32)]
+    )
+    email = StringField(
+        "Email",
+        validators=[DataRequired(), Email()]
+    )
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired(), Length(min=8, max=32)]
+    )
+    confirm_password = PasswordField(
+        "Confirm Password",
+        validators=[DataRequired(), EqualTo("password", message="Passwords didn't match.")]
+    )
+    submit = SubmitField("Sign up")
--- blog/users/routes.py
+++ blog/users/routes.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, render_template
-from blog.users.forms import LoginForm
+from blog.users.forms import LoginForm, RegisterForm
 
 users = Blueprint("users", __name__)
 
-@users.route("/login")
+@users.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
-    return render_template("users/login.html", form=form)
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        pass
+    return render_template("users/login.html", title="Login", form=login_form)
+
+@users.route("/register", methods=["GET", "POST"])
+def register():
+    register_form = RegisterForm()
+    if register_form.validate_on_submit():
+        pass
+    return render_template("users/register.html", title="Register", form=register_form)
