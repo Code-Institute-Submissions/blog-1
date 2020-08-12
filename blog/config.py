0b15a759ece747bfa7ff331f72d6f1498bdcc4ec
Yiaoma <a.o.kalugin@gmail.com>
2020-08-12 18:44:28 +0100

    Fixed modal position.

--- blog/config.py
+++ blog/config.py
@@ -5,4 +5,4 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     MONGODB_SETTINGS = {
     'host': "mongodb+srv://Yaoma:pfc0a7aNQiF22Lek@cluster0-xbf5e.mongodb.net/blog?retryWrites=true&w=majority"
-}
+    }
