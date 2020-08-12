7e2421c8febd959292ae3f78fddda3540c2f6b3c
Yiaoma <a.o.kalugin@gmail.com>
2020-08-12 18:11:33 +0100

    Added host and port variables to run.py.

--- blog/config.py
+++ blog/config.py
@@ -3,8 +3,6 @@ import os
 # Application configurations.
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    IP = os.environ.get("IP")
-    PORT = os.environ.get("PORT")
     MONGODB_SETTINGS = {
     'host': "mongodb+srv://Yaoma:pfc0a7aNQiF22Lek@cluster0-xbf5e.mongodb.net/blog?retryWrites=true&w=majority"
 }
--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+import os
 from blog import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        host=os.environ.get("IP"),
+        port=os.environ.get("PORT")
+    )
