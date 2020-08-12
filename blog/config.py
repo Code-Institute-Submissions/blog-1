a2e4f7aeea90e49bb87eb5198eac6946d22a6620
Yiaoma <a.o.kalugin@gmail.com>
2020-08-12 18:50:51 +0100

    Trying to hide mongodb connection string into enviromental variable.

--- blog/config.py
+++ blog/config.py
@@ -3,6 +3,4 @@ import os
 # Application configurations.
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    MONGODB_SETTINGS = {
-    'host': "mongodb+srv://Yaoma:pfc0a7aNQiF22Lek@cluster0-xbf5e.mongodb.net/blog?retryWrites=true&w=majority"
-    }
+    MONGODB_HOST = os.environ.get("MONGODB_HOST")
